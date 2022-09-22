76ab042903094ef36f8ccf39a9327ac4d8d0d94e
atakhan <aahmedov13464@ada.edu.az>
2022-09-22 10:50:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -466,3 +466,6 @@ const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 */
+
+// I am updating in local repo
+// I will soon publish to remote.
